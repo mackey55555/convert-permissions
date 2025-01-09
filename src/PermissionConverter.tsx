b92e04854f40e8269735551647a2ab21641de58b
mackey55555 <a.makihara@auris.jp>
2025-01-09 10:37:57 +0900

    tagu追加

--- src/PermissionConverter.tsx
+++ src/PermissionConverter.tsx
@@ -145,125 +145,163 @@ const PermissionConverter: React.FC = () => {
   }, [inputPermissionUpper]);
 
   useEffect(() => {
-    const textPermission = `${checkboxState.owner.read ? "r" : "-"}${
-      checkboxState.owner.write ? "w" : "-"
-    }${checkboxState.owner.execute ? "x" : "-"}${
-      checkboxState.group.read ? "r" : "-"
-    }${checkboxState.group.write ? "w" : "-"}${
-      checkboxState.group.execute ? "x" : "-"
-    }${checkboxState.public.read ? "r" : "-"}${
-      checkboxState.public.write ? "w" : "-"
-    }${checkboxState.public.execute ? "x" : "-"}`;
+    const textPermission = `${checkboxState.owner.read ? "r" : "-"}${checkboxState.owner.write ? "w" : "-"
+      }${checkboxState.owner.execute ? "x" : "-"}${checkboxState.group.read ? "r" : "-"
+      }${checkboxState.group.write ? "w" : "-"}${checkboxState.group.execute ? "x" : "-"
+      }${checkboxState.public.read ? "r" : "-"}${checkboxState.public.write ? "w" : "-"
+      }${checkboxState.public.execute ? "x" : "-"}`;
     const numericPermission = convertToNumeric(textPermission);
     setOutputTextLower(textPermission);
     setInputPermissionLower(numericPermission);
   }, [checkboxState]);
 
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ mt: 4, mb: 4 }}>
-        <Typography variant="h4" align="center" color="primary">
-          パーミッションプロフェッサーくん
-        </Typography>
+    <Box sx={{ display: "flex" }}>
+      {/* 左サイドバー */}
+      <Box
+        sx={{
+          width: "15%",
+          backgroundColor: "#f9f9f9",
+          p: 2,
+          textAlign: "center",
+        }}
+      >
+        <div id="ads-left">
+          <script
+            src="https://adm.shinobi.jp/s/8a76f3b6ab6d540ea99edc699fd00623"
+            async
+          ></script>
+        </div>
       </Box>
-      {/* パーミッション変換機能 */}
-      <Paper elevation={3} sx={{ p: 3, borderRadius: 2, mb: 4 }}>
-        <Box
-          component="form"
-          onSubmit={handleSubmitUpper}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-          }}
-        >
-          <TextField
-            label="パーミッションコードを入力"
-            variant="outlined"
-            value={inputPermissionUpper}
-            onChange={(e) => setInputPermissionUpper(e.target.value)}
-          />
-          <FormControl fullWidth variant="outlined">
-            <InputLabel htmlFor="conversion-type">変換タイプ</InputLabel>
-            <Select
-              label="変換タイプ"
-              id="conversion-type"
-              value={conversionType}
-              onChange={(e) =>
-                setConversionType(e.target.value as ConversionType)
-              }
-            >
-              <MenuItem value="textToNumeric">
-                テキストから数値へ (例: rwxr-xr-x)
-              </MenuItem>
-              <MenuItem value="numericToText">
-                数値からテキストへ (例: 755)
-              </MenuItem>
-            </Select>
-          </FormControl>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            sx={{ mt: 2 }}
-          >
-            変換
-          </Button>
-        </Box>
-        {outputPermissionUpper && (
-          <Box sx={{ mt: 4, display: "flex", flexDirection: "column", gap: 1 }}>
-            <Typography variant="h6">変換されたパーミッション:</Typography>
-            <Typography variant="body1" color="secondary">
-              {outputPermissionUpper}
-            </Typography>
-          </Box>
-        )}
-      </Paper>
 
-      {/* チェックボックス機能 */}
-      <Paper elevation={3} sx={{ p: 3, borderRadius: 2 }}>
-        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-          <Typography variant="h4" align="center" color="primary">
-            チェックボックスでパーミッション設定
-          </Typography>
-          <Divider />
-          <FormGroup>
-            {["owner", "group", "public"].map((section) => (
-              <Box key={section}>
-                <Typography variant="subtitle1">{section}:</Typography>
-                {["read", "write", "execute"].map((permission) => (
-                  <FormControlLabel
-                    key={permission}
-                    control={
-                      <Switch
-                        checked={
-                          checkboxState[section as keyof CheckboxState][
-                            permission as keyof typeof checkboxState["owner"]
-                          ]
-                        }
-                        onChange={handleCheckboxChange}
-                        name={`${section}.${permission}`}
-                      />
-                    }
-                    label={permission}
-                  />
-                ))}
-              </Box>
-            ))}
-          </FormGroup>
-          <Box sx={{ mt: 4, display: "flex", flexDirection: "column", gap: 1 }}>
-            <Typography variant="h6">現在のパーミッション設定:</Typography>
-            <Typography variant="body1" color="secondary">
-              数値: {inputPermissionLower}
-            </Typography>
-            <Typography variant="body1" color="secondary">
-              テキスト: {convertToText(inputPermissionLower)}
+      {/* メインコンテンツ */}
+      <Box sx={{ flex: 1 }}>
+        <Container maxWidth="sm">
+          <Box sx={{ mt: 4, mb: 4 }}>
+            <Typography variant="h4" align="center" color="primary">
+              パーミッションプロフェッサーくん
             </Typography>
           </Box>
-        </Box>
-      </Paper>
-    </Container>
+          {/* コンテンツここに配置 */}
+          {/* パーミッション変換機能 */}
+          <Paper elevation={3} sx={{ p: 3, borderRadius: 2, mb: 4 }}>
+            <Box
+              component="form"
+              onSubmit={handleSubmitUpper}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+              }}
+            >
+              <TextField
+                label="パーミッションコードを入力"
+                variant="outlined"
+                value={inputPermissionUpper}
+                onChange={(e) => setInputPermissionUpper(e.target.value)}
+              />
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="conversion-type">変換タイプ</InputLabel>
+                <Select
+                  label="変換タイプ"
+                  id="conversion-type"
+                  value={conversionType}
+                  onChange={(e) =>
+                    setConversionType(e.target.value as ConversionType)
+                  }
+                >
+                  <MenuItem value="textToNumeric">
+                    テキストから数値へ (例: rwxr-xr-x)
+                  </MenuItem>
+                  <MenuItem value="numericToText">
+                    数値からテキストへ (例: 755)
+                  </MenuItem>
+                </Select>
+              </FormControl>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                sx={{ mt: 2 }}
+              >
+                変換
+              </Button>
+            </Box>
+            {outputPermissionUpper && (
+              <Box sx={{ mt: 4, display: "flex", flexDirection: "column", gap: 1 }}>
+                <Typography variant="h6">変換されたパーミッション:</Typography>
+                <Typography variant="body1" color="secondary">
+                  {outputPermissionUpper}
+                </Typography>
+              </Box>
+            )}
+          </Paper>
+
+          {/* チェックボックス機能 */}
+          <Paper elevation={3} sx={{ p: 3, borderRadius: 2 }}>
+            <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+              <Typography variant="h4" align="center" color="primary">
+                チェックボックスでパーミッション設定
+              </Typography>
+              <Divider />
+              <FormGroup>
+                {["owner", "group", "public"].map((section) => (
+                  <Box key={section}>
+                    <Typography variant="subtitle1">{section}:</Typography>
+                    {["read", "write", "execute"].map((permission) => (
+                      <FormControlLabel
+                        key={permission}
+                        control={
+                          <Switch
+                            checked={
+                              checkboxState[section as keyof CheckboxState][
+                              permission as keyof typeof checkboxState["owner"]
+                              ]
+                            }
+                            onChange={handleCheckboxChange}
+                            name={`${section}.${permission}`}
+                          />
+                        }
+                        label={permission}
+                      />
+                    ))}
+                  </Box>
+                ))}
+              </FormGroup>
+              <Box sx={{ mt: 4, display: "flex", flexDirection: "column", gap: 1 }}>
+                <Typography variant="h6">現在のパーミッション設定:</Typography>
+                <Typography variant="body1" color="secondary">
+                  数値: {inputPermissionLower}
+                </Typography>
+                <Typography variant="body1" color="secondary">
+                  テキスト: {convertToText(inputPermissionLower)}
+                </Typography>
+              </Box>
+            </Box>
+          </Paper>
+        </Container>
+      </Box>
+
+      {/* 右サイドバー */}
+      <Box
+        sx={{
+          width: "15%",
+          backgroundColor: "#f9f9f9",
+          p: 2,
+          textAlign: "center",
+        }}
+      >
+        <div id="ads-right">
+          <script
+            src="https://adm.shinobi.jp/s/8a76f3b6ab6d540ea99edc699fd00623"
+            async
+          ></script>
+        </div>
+      </Box>
+    </Box>
   );
 };
+
+
 
 export default PermissionConverter;
